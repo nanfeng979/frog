5e2f881f60f7c62342a58476195f323b350e1ad0
nanfeng979 <306338401@qq.com>
2023-04-27 20:14:05 +0800

    fix UI之间不能重复展开

--- Scripts/UI/Illu/IlluController.cs
+++ Scripts/UI/Illu/IlluController.cs
@@ -7,12 +7,25 @@ public class IlluController : MonoBehaviour
     [SerializeField] private GameObject IlluFrame;
 
     public void OnClick() {
+        if(CantContinue()) {
+            return;
+        }
+
         IlluFrame.SetActive(true);
         GameManager.Instance.SetGameState(EGameState.PopUp);
     }
 
     public void OnClickClose() {
+
         IlluFrame.SetActive(false);
         GameManager.Instance.SetGameStateUsePrev();
+    }
+
+    private bool CantContinue() {
+        if(GameManager.Instance.GameState == EGameState.PopUp) {
+            return true;
+        }
+
+        return false;
     }
 }
--- Scripts/UI/Return/Return.cs
+++ Scripts/UI/Return/Return.cs
@@ -7,10 +7,21 @@ public class Return : MonoBehaviour
     [SerializeField] private GameObject returnConfirm;
 
     public void EnableReturnConfirm() {
+        if(CantContinue()) {
+            return;
+        }
+
         if(returnConfirm.gameObject.activeSelf == false) {
             returnConfirm.gameObject.SetActive(true);
             GameManager.Instance.SetGameState(EGameState.PopUp);
         }
+    }
+
+    private bool CantContinue() {
+        if(GameManager.Instance.GameState == EGameState.PopUp) {
+            return true;
+        }
+        return false;
     }
 
 }
